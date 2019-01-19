af5b5f51a36f35a2b017795cb0d5a37243c1dc73
unknown <ikdiscipulo@gmail.com>
2019-01-19 14:03:52 -0700

    input changes

--- src/input.js
+++ src/input.js
@@ -3,11 +3,8 @@
 //   input: input string from SMS
 // Returns:
 //
-const twilio = require('twilio');
-const google_maps = require('google_maps');
-
-// const twilio = require('./twilio');
-const google_maps = require('./src/google_maps.js');
+const twilio = require('./twilio');
+const google_maps = require('./google_maps');
 const reg = /(?:(?:(?:(?:F|f)rom (.+)) (?:(?:T|t)o (.+)))|(?:(?:(?:T|t)o (.+)) (?:(?:F|f)rom (.+))))/g
 
  async function parse(input) {
@@ -22,11 +19,11 @@ const reg = /(?:(?:(?:(?:F|f)rom (.+)) (?:(?:T|t)o (.+)))|(?:(?:(?:T|t)o (.+)) (
     }
 
   twilio.sendMessage(message, number);
-  return groups;
+  // return groups;
 }
 
 module.exports = {
   parse
 }
 
-console.log(parse('Go from 343 e North st to 493 N 16th st'))
+// console.log(parse('Go from 343 e North st to 493 N 16th st'))
