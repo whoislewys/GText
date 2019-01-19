677b33756478b5c1e7109f6e3bcd7aecb308f11f
Sam Rondinelli <sam.rondinelli3@gmail.com>
2019-01-19 00:03:54 -0700

    Added project layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({message: "This is the only response that the server can give right now"})
+});
+
+router.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  if (req.body.Body == 'hello') {
+    twiml.message('Hi!');
+  } else if (req.body.Body == 'bye') {
+    twiml.message('Goodbye');
+  } else {
+    twiml.message(
+      'No Body param match, Twilio sends this in the request to your server.'
+    );
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
 });
 
 module.exports = router;
---
+++ src/twilio.js
@@ -0,0 +1,34 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const fromNumber = '***REMOVED***';
+const client = require('twilio')(accountSid, authToken);
+
+// Send a message using the Twilio API
+// Params:
+//   messageText: input message to send
+//   number: phone number to send the text to
+// Returns:
+//   void
+function sendMessage(messageText, number) {
+  client.messages
+    .create({
+       body: messageText,
+       from: fromNumber,
+       to: number
+     })
+    .then(message => console.log(message.sid))
+    .done();
+}
+
+// Receive message from Twilio webhook
+// Params:
+//
+//
+// Returns:
+//
+//
+function receiveMessage() {
+
+}
