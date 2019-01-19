af6909ebcafa7eaa8a7b6c0dc510301529720112
Sam Rondinelli <sam.rondinelli3@gmail.com>
2019-01-18 23:44:53 -0700

    Cleaned up server code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({message: "This is the only response that the server can give right now"})
 });
 
 module.exports = router;
