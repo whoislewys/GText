9f0eb7b8498f8ea3bf5f6455bdef9038559c0c1b
Sam Rondinelli <sam.rondinelli3@gmail.com>
2019-01-19 14:35:43 -0700

    Added more robust error handling

--- src/input.js
+++ src/input.js
@@ -8,24 +8,21 @@ const google_maps = require('./google_maps');
 const reg = /(?:(?:(?:(?:F|f)rom (.+)) (?:(?:T|t)o (.+)))|(?:(?:(?:T|t)o (.+)) (?:(?:F|f)rom (.+))))/g
 
  async function parse(message_in, number) {
+  let message_out = "Sorry, we couldn't understand that! Try 'directions from *location* to *destination*.'";
   let groups = reg.exec(message_in)
-    if (groups) {
-      if (groups[1]) {
-        var message_out = await google_maps.get_directions(groups[1], groups[2]);
-      } else {
-        var message = await google_maps.get_directions(groups[3], groups[4]);
-      }
+
+  if (groups) {
+    if (groups[1]) {
+      message_out = await google_maps.get_directions(groups[1], groups[2]);
     } else {
-      message_out = "Sorry, we couldn't understand that! Try 'directions from *location* to *destination*.'"
+      message_out = await google_maps.get_directions(groups[3], groups[4]);
     }
+  }
 
   console.log(message_out);
   twilio.sendMessage(message_out, number);
-  // return groups;
 }
 
 module.exports = {
   parse
 }
-
-// console.log(parse('Go from 343 e North st to 493 N 16th st'))
