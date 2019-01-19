b33a7f809eb6921b374f378b54957e9b9f41bbbc
unknown <ikdiscipulo@gmail.com>
2019-01-19 13:58:43 -0700

    added google maps to input.js

--- src/input.js
+++ src/input.js
@@ -3,11 +3,10 @@
 //   input: input string from SMS
 // Returns:
 //
-const twilio = require('./twilio');
-const google_maps = require('./google_maps');
+const twilio = require('twilio');
+const google_maps = require('google_maps');
 
-async function parse(input) {
-
+async function parse(input) { 
   var message = await google_maps.get_directions(origin, destination);
   twilio.sendMessage(message, number);
 }
