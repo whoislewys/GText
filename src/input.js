d6d9fd0681747dfdd36d815bc85383f8435b161c
Sam Rondinelli <sam.rondinelli3@gmail.com>
2019-01-19 17:15:57 -0700

    Added search functionality

--- src/input.js
+++ src/input.js
@@ -5,11 +5,14 @@
 //
 const twilio = require('./twilio');
 const google_maps = require('./google_maps');
+const google_search = require('./google_search');
 const reg = /(?:(?:(?:(?:F|f)rom (.+)) (?:(?:T|t)o (.+)))|(?:(?:(?:(?:T|t)o(?!.*(?:T|t)o)) (.+)) (?:(?:F|f)rom (.+))))/g
+const searchReg = /(?:(?:S|s)earch) (.+)/g
 
  async function parse(message_in, number) {
   let message_out = "Sorry, we couldn't understand that! Try 'directions from *location* to *destination*.'";
-  let groups = reg.exec(message_in)
+  let groups = reg.exec(message_in);
+  let search = searchReg.exec(message_in);
 
   if (groups) {
     if (groups[1]) {
@@ -17,10 +20,12 @@ const reg = /(?:(?:(?:(?:F|f)rom (.+)) (?:(?:T|t)o (.+)))|(?:(?:(?:(?:T|t)o(?!.*
     } else {
       message_out = await google_maps.get_directions(groups[3], groups[4]);
     }
+  } else if(search) {
+    message_out = await google_search.get_result(search[1]);
   }
 
   console.log(message_out);
-  // twilio.sendMessage(message_out, number);
+  twilio.sendMessage(message_out, number);
 }
 
 module.exports = {
