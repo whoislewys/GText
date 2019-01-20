6873547e5a7e04a8a93fb7e7cc0a0d983642aac7
unknown <ikdiscipulo@gmail.com>
2019-01-19 18:44:44 -0700

    fixed space delim

--- src/google_search.js
+++ src/google_search.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-const base_search = 'https://www.googleapis.com/customsearch/v1?key=***REMOVED***&cx=013537525589528080147:vd3pceyhst8&q='
+const base_search = 'https://www.googleapis.com/customsearch/v1?key=***REMOVED***&cx=013537525589528080147:vd3pceyhst8&lr=lang_en&q='
 
 function clean_result(result) {
 	var out = result.data.items[0].snippet;
@@ -8,7 +8,7 @@ function clean_result(result) {
 }
 
 function get_result(keywords) {
-	var keywords_search = keywords.replace(' ', '&');
+	var keywords_search = keywords.replace(' ', '+');
 	return axios.get(base_search + keywords_search)
 		.then(response => clean_result(response))
 		.catch((err) => {
